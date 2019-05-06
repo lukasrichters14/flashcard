f6eac3008572199a9603bd36118e98920aa00373
Lukas <lukasrichters14@gmail.com>
2019-05-06 15:09:24 -0400

    The flashcard program is completely functional and passes all test cases from the test.txt file.

--- FlashcardProgramV2.py
+++ FlashcardProgramV2.py
@@ -20,37 +20,6 @@ def open_files():
     
     return read_file, write_file
 
-def format_simple(line, split_char):
-    line = line
-    split_char = split_char
-    # Find where the question ends.
-    question_end = find_last_of(split_char, line) + 1
-    # Split string into question and answer.
-    question = line[:question_end].lower().strip().capitalize()
-    answer = line[question_end:].lower().strip().capitalize()
-    
-    # If the last character of the answer is not a period, add a period.
-    if answer[-1] != ".":
-        answer += "."
-        
-    # Capitalize each letter after a period in the question.
-    for ind, ch in enumerate(question):
-        if ch == "." and ind != (len(question)-1):
-            question = question[:ind+1] + " " + question[ind+2].capitalize() + question[ind+3:]
-        
-    # Capitalize each letter after a period in the answer.
-    for ind2, ch2 in enumerate(answer):
-        if ch2 == "." and ind2 != (len(answer)-1):
-            answer = answer[:ind2+1] + " " +answer[ind2+2].capitalize() + answer[ind2+3:]            
-    
-    # Add an @ symbol to the end of the question to differentiate between
-    # question and answer on quizlet.
-    question += "@"
-    
-    # Put everything back together to output.
-    final_output = question + answer
-        
-    return final_output
 
 def extract_data(file):
     '''Takes a file as input and puts each line into a list.'''
@@ -58,6 +27,10 @@ def extract_data(file):
     
     for line in file:
         return_list.append(line)
+    
+    # Appends a $ to the end of the list so that the end of the document can be
+    # easily found.
+    return_list.append("$")
     
     return return_list
 
@@ -79,6 +52,8 @@ def find_last_of(string, char):
 def is_simple(line):
     '''Returns True if the line is simple, false otherwise.'''
     
+    line = line.strip()
+    
     if ("-" in line) and (line[-1] != "-"):
         return True
     
@@ -94,6 +69,8 @@ def is_simple(line):
 
 def is_complex(line):
     '''Returns True if the line is complex, false otherwise.'''
+    
+    line = line.strip()
     
     if "-" == line[-1]:
         return True
@@ -111,7 +88,9 @@ def is_complex(line):
 def is_neither(line):
     '''Returns True if the line is neither complex nor simple, false otherwise.'''
     
-    if(not is_complex(line)) and (not is_simple(line)):
+    line = line.strip()
+    
+    if (not is_complex(line)) and (not is_simple(line)):
         return True
     else:
         return False
@@ -128,82 +107,98 @@ def organize_data(file_list):
     
     true_index = 0   # Keeps track of the index of the most recent line.
     
-    # Loop through the file list.
+    # Loops through the file list.
     for i, line in enumerate(file_list):
         if i == true_index:
+            true_index += 1
             if "-" in line:
                 # Question is one line, answer is another.
                 if is_complex(line):
-                    question = line.strip()
+                    # Removes the last character and excess whitespace.
+                    question = line[:-1].strip()
                     answer = ""
                     
                     # Continues to add lines to the answer until a new question
-                    # is found.
-                    while is_neither(file_list[true_index]):
-                        answer += file_list[true_index].strip() + "/n"
+                    # is found or the end of the list is found.
+                    while is_neither(file_list[true_index]) and file_list[true_index] != "$":
+                        answer += file_list[true_index].strip() + "\n"
                         true_index += 1
                     
-                    true_index -= 1  # Makes sure not to skip the next line.
+                    answer = answer[:-1]
                     
                 # Question and answer are on the same line.
                 else:
+                    # Finds where the question ends and answer begins, then
+                    # seperates them.
                     split_index = find_last_of(line, "-")
                     question = line[:split_index].strip()
                     answer = line[split_index + 1:].strip()
                 
+                # Adds the questions and answers to a dictionary.
                 q_a_dict[question] = answer
             
             elif ":" in line:
                 # Question is one line, answer is another.
                 if is_complex(line):
-                    question = line.strip()
+                    # Removes the last character and excess whitespace.
+                    question = line[:-1].strip()
                     answer = ""
                     
                     # Continues to add lines to the answer until a new question
-                    # is found.
-                    while is_neither(file_list[true_index]):
-                        answer += file_list[true_index].strip() + "/n"
+                    # is found or the end of the list is found.
+                    while is_neither(file_list[true_index]) and file_list[true_index] != "$":
+                        answer += file_list[true_index].strip() + "\n"
                         true_index += 1
                     
-                    true_index -= 1  # Makes sure not to skip the next line.
-                
+                    answer = answer[:-1]
+                    
                 # Question and answer are on the same line.
                 else:
+                    # Finds where the question ends and answer begins, then
+                    # seperates them.
                     split_index = find_last_of(line, ":")
                     question = line[:split_index].strip()
                     answer = line[split_index + 1:].strip()
                 
+                # Adds the questions and answers to a dictionary.
                 q_a_dict[question] = answer
 
             elif "?" in line:
                 # Question is one line, answer is another.
                 if is_complex(line):
-                    question = line.strip()
+                    # Removes the last character and excess whitespace.
+                    question = line[:-1].strip()
                     answer = ""
                     
                     # Continues to add lines to the answer until a new question
-                    # is found.
-                    while is_neither(file_list[true_index]):
-                        answer += file_list[true_index].strip() + " & "
+                    # is found or the end of the list is found.
+                    while is_neither(file_list[true_index]) and file_list[true_index] != "$":
+                        answer += file_list[true_index].strip() + "\n"
                         true_index += 1
                     
-                    true_index -= 1  # Makes sure not to skip the next line.
+                    answer = answer[:-1]
                 
                 # Question and answer are on the same line.
                 else:
+                    # Finds where the question ends and answer begins, then
+                    # seperates them.
                     split_index = find_last_of(line, "?")
                     question = line[:split_index].strip()
                     answer = line[split_index + 1:].strip()
                 
+                # Adds the questions and answers to a dictionary.
                 q_a_dict[question] = answer
+            
+            # Keeps the end of list character from interfering with normal
+            # operations.
+            elif line == "$":
+                pass
             
             # Basic message to use to signify that there is not a '-', ':', or 
             # '?' character in the given line.
             else:
                 print("Unrecognized symbol in line " + str(i + 1) + ".")
                 print("Line is:", line)
-        
-        true_index += 1
     
     return q_a_dict
 
@@ -215,21 +210,25 @@ def output_data(file_dict, out_file):
     file_dict = file_dict
     out_file = out_file
     
-    # Writes the question and answer to the output file. No formatting...yet.
+    # Writes the question and answer to the output file. This has the basic
+    # formatting, only enough to differentiate the question and answer and
+    # seperate cards.
     for q, a in file_dict.items():
-        print("{}@{}".format(q, a), file = out_file)
+        print("{}@{}~!".format(q, a), file = out_file)
 
     
 def main():
     '''This is the overall program.'''
-    # Open the two files.
+    # Opens the read and write files.
     read_file, write_file = open_files()
     
-    # Get all the lines in the file.
+    # Gets all the lines in the file.
     file_list = extract_data(read_file)
     
+    # Gets a dictionary of all of the questions and answers.
     file_dict = organize_data(file_list)
     
+    # Writes to the write file.
     output_data(file_dict, write_file)
                 
     read_file.close()
